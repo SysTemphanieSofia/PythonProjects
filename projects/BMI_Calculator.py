b3da539469b0859c6035a9a3be5bf8092e7a389f
unknown <sy.stephaniesofia@gmail.com>
2026-06-16 17:27:22 +0800

    test

---
+++ projects/BMI_Calculator.py
@@ -0,0 +1,16 @@
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+# 🚨 Do not modify the values above
+# Write your code below 👇
+
+if bmi < 18.5:
+    print("underweight")
+    
+elif 18.5 <= bmi > 24:
+    print("normal weight")
+    
+else:
+    print("overweight")
--- test.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name: ")
-count_name = len(name)
-
-print("Number of letters in your name: " + str(count_name))
